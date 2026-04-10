b8c4766e52f7da443933fc42981045045a5ea7a0
fra.acosta@duocuc.cl <fra.acosta@duocuc.cl>
2026-04-10 17:19:54 -0400

    Corregir mensaje de estado en el microservicio

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
 
     res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-    res.end('✅ Microservicio de Gestión de Usuarios: Operativo y Funcionando.\n');
+    res.end('✅ Microservicio de Gestión de Usuarios: Operativo y Funcionando Hotfix Aplicado.\n');
 });
 
 const PORT = 3000;
